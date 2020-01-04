7b7a8a35b9a0bd207e4ea4283dc4f11b4af98965
root <matheus_mendess@live.com>
2020-01-04 15:44:22 -0300

    todolist :memo::memo::memo::memo::memo:

---
+++ src/Todolist.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+export default class Todolist extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            items:[],
+            todo: ''
+        }
+    }
+
+    onChange = (event) => {
+        this.setState({todo: event.target.value});
+    }
+
+    clickParaEnviar = (event) => {
+        event.preventDefault();
+        this.setState({
+            items: [...this.state.items, this.state.todo],
+            todo: ''
+        });
+    }
+    render(){
+        const {items} = this.state;
+
+        return(
+            <div>
+                <input type="text" name="todo" placeholder="Entre com seu todo" onChange={this.onChange}/>
+                <button type="button" onClick={this.clickParaEnviar}>Enviar</button>
+                <hr/>
+
+                <ul>
+                    {items.map((item, index) => (
+                        <li key={index}>{item}</li>
+                    ))}   
+                </ul>
+            </div>
+        )
+    }
+}
